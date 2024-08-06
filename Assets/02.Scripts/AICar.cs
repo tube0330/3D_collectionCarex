071b473edeb3cba6f7a8ed7b01bbe01279257911
tube0330 <zien0516@naver.com>
2024-08-06 17:57:06 +0900

    240806

--- Assets/02.Scripts/AICar.cs
+++ Assets/02.Scripts/AICar.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class AICar : MonoBehaviour
 {
+    [Header("Center of Mass")]
+    [SerializeField]Rigidbody rb;
+    public Vector3 CenterOfMass = new Vector3(0, -0.5f, 0);
+
+    [Header("wheel Colliders")]
+    [SerializeField] WheelCollider Front_L;
+    [SerializeField] WheelCollider Front_R;
+    [SerializeField] WheelCollider Back_L;
+    [SerializeField] WheelCollider Back_R;
+    [Header("Modeling")]
+
+    [Header("Path")]
     [SerializeField] Transform path;
     [SerializeField] Transform[] pathTransforms;
+    [SerializeField] List<Transform> pathList;
+
+    public float curSpeed = 0f;
+    private float maxSpeed = 100f;
+    private int point = 0;
 
     void Start()
     {
