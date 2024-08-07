9c52cd6b3fcdd6d34b709938eb2aa411a579b492
tube0330 <zien0516@naver.com>
2024-08-07 09:02:13 +0900

    240807_AICar

--- Assets/02.Scripts/AICar.cs
+++ Assets/02.Scripts/AICar.cs
@@ -24,14 +24,65 @@ public class AICar : MonoBehaviour
     private float maxSpeed = 100f;
     private int point = 0;
 
+    public float maxMotorTorque = 1000f;
+    public float maxSteerAngle = 30f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = CenterOfMass;
+        path = GameObject.Find("PathTransform").transform;
+        pathTransforms = path.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != path)
+                pathList.Add(pathTransforms[i]);
+        }
+    }
+void FixedUpdate()
+    {
+        ApplySteer();
+        Drive();
+        CheckWayPointDistance();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ApplySteer()
     {
-        
+        Vector3 relativeVector = transform.InverseTransformPoint(pathList[point].position);
+
+        float newSteer = relativeVector.x / relativeVector.magnitude * maxSteerAngle;
+
+        Front_L.steerAngle = newSteer;
+        Front_R.steerAngle = newSteer;
+    }
+
+    void Drive()
+    {
+        curSpeed = 2 * Mathf.PI * Front_L.radius * Front_L.rpm * 60 / 1000;
+
+        if (curSpeed < maxSpeed)
+        {
+            Back_L.motorTorque = maxMotorTorque;
+            Back_R.motorTorque = maxMotorTorque;
+        }
+
+        else
+        {
+            Back_L.motorTorque = 0;
+            Back_R.motorTorque = 0;
+        }
+    }
+
+    void CheckWayPointDistance()
+    {
+        Debug.Log(point);
+        if(Vector3.Distance(transform.position, pathList[point].position) <= 10f)
+        {
+            if(point == pathList.Count - 1)
+            point = 0;
+
+            else point++;
+        }
     }
 }
